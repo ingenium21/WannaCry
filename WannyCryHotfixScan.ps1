c01abea84d9ce72f05e75c9c4a807d8980c07017
Ren <renato.regalado@gmail.com>
2017-05-17 12:00:05 -0500

    Update WannyCryHotfixScan.ps1

--- WannyCryHotfixScan.ps1
+++ WannyCryHotfixScan.ps1
@@ -1,5 +1,5 @@
-#Version 1.08.00 @KieranWalsh May 2017
-# Computer Talk LTD
+#Version 1.08.00 @ingenium21 May 2017
+# Author: Renato Regalado
 
 # Thanks to https://github.com/TLaborde, and https://www.facebook.com/BlackV for notifying me about missing patches.
 
